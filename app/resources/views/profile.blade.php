1d3ccb8c3a22c4f0ea8165924ae311d6909299cf
davidao06 <a93274@alunos.uminho.pt>
2022-07-01 23:11:17 +0100

    Autenticaçao e perfis implementados com interface simples

---
+++ app/resources/views/profile.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="css/profile.css" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="profileContainer">
+            <div class="topBar">
+                <span class="welcomeProfile">Olá {{Auth::user()->username}}!</span>
+                <form action="{{route('logout.user')}}" method="get">
+                    @csrf
+                    <input type="submit" value="Logout" class="logoutButton">
+                </form>
+            </div>
+
+            <div class="messageContainer">
+                <div class="message">
+                    <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet illo dignissimos rem consequuntur, repellendus vero sapiente perferendis odit quia, quisquam, similique cum accusamus nisi voluptas. Expedita harum veritatis illum rem!</span>
+                </div>
+
+                <div class="message">
+                    <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet illo dignissimos rem consequuntur, repellendus vero sapiente perferendis odit quia, quisquam, similique cum accusamus nisi voluptas. Expedita harum veritatis illum rem!</span>
+                </div>
+
+                <div class="message">
+                    <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet illo dignissimos rem consequuntur, repellendus vero sapiente perferendis odit quia, quisquam, similique cum accusamus nisi voluptas. Expedita harum veritatis illum rem!</span>
+                </div>
+
+                <div class="message">
+                    <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet illo dignissimos rem consequuntur, repellendus vero sapiente perferendis odit quia, quisquam, similique cum accusamus nisi voluptas. Expedita harum veritatis illum rem!</span>
+                </div>
+
+                <div class="message">
+                    <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet illo dignissimos rem consequuntur, repellendus vero sapiente perferendis odit quia, quisquam, similique cum accusamus nisi voluptas. Expedita harum veritatis illum rem!</span>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
